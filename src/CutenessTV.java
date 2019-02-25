74a81124e91e818c1352644556b70cccda5bbe60
harrisoncliew <league@WTS-IM-17.attlocal.net>
2019-02-25 15:52:38 -0800

    CutenessTV

--- src/CutenessTV.java
+++ src/CutenessTV.java
@@ -5,25 +5,42 @@ import java.net.URI;
 import javax.swing.*;
 public class CutenessTV implements ActionListener{
 public static void main(String[] args) {
-	
+	CutenessTV tv = new CutenessTV();
+	tv.setup();
 }
 JButton button1 = new JButton();
 JButton button2 = new JButton();
 JButton button3 = new JButton();
+JPanel panel = new JPanel();
+JFrame frame = new JFrame();
 
 void setup() {
 	button1.setText("Frog Video");
 	button2.setText("Duck Video");
 	button3.setText("Fluffy Unicorn Video");
+	button1.addActionListener(this);
+	button2.addActionListener(this);
+	button3.addActionListener(this);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(panel);
+	panel.add(button1);
+	panel.add(button2);
+	panel.add(button3);
+	frame.pack();
+	frame.setVisible(true);
 }
 @Override
 public void actionPerformed(ActionEvent arg0) {
 	JButton buttonPressed = (JButton) arg0.getSource();
 	if(buttonPressed==button1) {
-		showFrogs();
+		showFrog();
 	}
-	if(buttonPressed==button2)
-	
+	if(buttonPressed==button2) {
+		showDucks();
+	}
+	if(buttonPressed==button3) {
+		showFluffyUnicorns();
+	}
 }
 void showDucks() {
     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
